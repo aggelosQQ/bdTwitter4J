e610be96ee7587e29042a2d1bd61926aa437f413
aggelosqq <aggelosqq@gmail.com>
2021-01-11 20:12:33 +0200

    First commit

--- src/bdTwitter.java
+++ src/bdTwitter.java
@@ -1,7 +1,6 @@
 import java.sql.*;
 import com.vdurmont.emoji.*;
 import twitter4j.*;
-
 import java.util.Date;
 
 public class bdTwitter {
@@ -10,12 +9,10 @@ public class bdTwitter {
         System.out.println("Connecting to database...");
         try (Connection connection = DriverManager.getConnection(sqlConn.url, sqlConn.username, sqlConn.password)) {
             System.out.println("Successfully connected to database!");
-
             Twitter twitter = TwitterFactory.getSingleton();
-            Query query = new Query("#panathinaikos"); // Search input - where it will retrieve the tweets from.
+            Query query = new Query("#euroleague"); // Search input - where it will retrieve the tweets from.
             query.setCount(50); // Amount of results to return per page.
             QueryResult result = twitter.search(query);
-
             Statement st = connection.createStatement();
 
             int counter = 0;
@@ -40,11 +37,6 @@ public class bdTwitter {
                 } catch (SQLException e) {
                     System.out.println("Something went wrong! (" + TweetID + " by " + displayName + ":" + userName + ") " + e);
                 }
-
-//                if (result.hasNext()) {
-//                    query = result.nextQuery();
-//                    result = twitter.search(query);
-//                }
             }
 
             System.out.printf("Inserted %d results to the database.", counter);
